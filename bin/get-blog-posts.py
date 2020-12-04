82dfbfd70645b941e354f2885fb867c59ed04163
qinliangchao <qinliangchao@timework.cn>
2020-12-04 14:05:40 +0800

    [IMP]featured value

--- bin/get-blog-posts.py
+++ bin/get-blog-posts.py
@@ -8,6 +8,7 @@ import time
 import hashlib
 import shutil
 import sys
+import random
 
 NOTION_TOKEN = os.getenv('NOTION_TOKEN')
 NOTION_ROOT_PAGE_ID = os.getenv('NOTION_ROOT_PAGE_ID')
@@ -29,7 +30,7 @@ ignore_root = True
 
 
 def download_file(file_url, destination_folder):
-    r = requests.get(file_url, stream=True,verify=False)
+    r = requests.get(file_url, stream=True, verify=False)
     # converts response headers mime type to an extension (may not work with everything)
     ext = r.headers['content-type'].split('/')[-1]
 
@@ -119,8 +120,9 @@ def to_markdown(page_id, ignore):
     if hasattr(page, 'cover') and page.cover:
         page_cover_url = 'https://www.notion.so' + page.cover
         cover_image_name = download_file(page_cover_url, dest_path)
-        print("cover_image_name :",cover_image_name)
-        metas.append(f"featured: '{cover_image_name}'")
+        featured = random.random()
+        print("cover_image_name :", cover_image_name)
+        metas.append(f"featured: '{featured}'")
 
     text, child_metas = process_block(page)
 
